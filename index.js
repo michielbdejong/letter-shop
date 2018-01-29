f996b90bd19ca0d680727f339051141c29df5e5c
Michiel de Jong <michiel@ripple.com>
2018-01-29 12:41:52 +0100

    switch to latest koa-ilp
    s

---
+++ index.js
@@ -0,0 +1,36 @@
+const http = require('http')
+const Koa = require('koa')
+const router = require('koa-router')()
+const parser = require('koa-bodyparser')()
+const Ilp = require('koa-ilp')
+const Plugin = require('ilp-plugin-mini-accounts')
+
+const server = http.createServer()
+server.listen(process.env.PORT)
+const plugin = new Plugin({
+  wsOpts: {
+    server
+  },
+  debugHostIldcpInfo: {
+    clientAddress: 'g.letter-shop',
+    assetScale: 6,
+    assetCode: 'XRP'
+  }
+})
+plugin.connect().then(() => {
+  console.log('Now run `PORT=' + process.env.PORT + ' node ./pay.js`')
+})
+
+const price = 1000
+const ilp = new Ilp({ plugin })
+router.options('/', ilp.options({ price }))
+router.get('/', ilp.paid({ price }), async ctx => {
+  ctx.body = { message: 'Hello World!' }
+})
+
+const app = new Koa()
+app
+  .use(parser)
+  .use(router.routes())
+  .use(router.allowedMethods())
+server.on('request', app.callback())
